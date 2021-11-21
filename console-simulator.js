d458b3579f3613cdbe8e34ef0d7f113162bf2a09
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2021-11-21 12:53:07 -0600

    This is a long commit. i was not sure how to implement these things, so I had to create a bunch of things and tweak them until the whole thing worked. I found some bugs, but due to their nature, I think it would be more appropriate to fix them in the master branch, once this one is merged. I implemented:   * a set of all recognized exit commands and modified the keyboard listener to use this set   * create a console_commands object and a recognized console command object that work the exact same way as the exit commands.     This are the commands that only the console simulator uses and are not related to the simulator.     The console_commands obj is to be used as a map between the textual commands the console recognizes and their handler.     The members of console_command literally correspond to the command recognized by the console, because they are used to create       the recognized_console_commands set.     Any mismatch between the console command and the name of the member in console_commands will result in an error. This could       happen if there is a mismatch in the documentation.   * created a handle_console_command function that abstracts away how console commands are handled   * created console command show_registers to see the registers in the console. This had to be done because since the code resides inside the readline       async fn, it cannot be accessed by the debugger. I already have a strategy to debug code that might reside inside async calls of external code.       Contact me if you want to know more.   * modified recursiveAsyncReadLine() to do the full thing of taking in an input and using the simulator to execute it. It was a good thing to have the       environment be an external variable. Without this, it would have been impossible to implement the console commands. iot was a good thing, but it       happened by accident. Right now the issue if that the parsing is done twice: once to check if the opt is valid, and another once when the code execute.       Either we change the execute fn (which I don't want to do) or we incorporate that check inside the simulator (which I think would be best). Doing so       would greatly improve the compleixty of the current code, but this requires an error API between the simulator and the outside.

--- console-simulator.js
+++ console-simulator.js
@@ -1,5 +1,6 @@
 const simulator = require('./simulator'); console.log(simulator.test_msg);
 const readline = require("readline");
+const { env } = require('yargs');
 
 // reader configuration
 const rl = readline.createInterface({
@@ -12,19 +13,37 @@ rl.on( "close", function () {
     process.exit(0);
 });
 
+// Exit commands
+exit_commands = new Set();
+['exit','quit','q','close'].forEach( e => exit_commands.add( e ) );
+
+// Console commands
+// This object maps the textual command to the fn obj that handles it
+console_commands = {
+    show_registers: show_registers
+}
+
+//~populates set obj
+recognized_console_commands = new Set();
+for( const property in console_commands){
+    recognized_console_commands.add( `${ property }` );
+}
+
+
 // simulation configuration
 //~ needs input validation
 //~ how does this not break the stack? I think it does, right? This shit can only handle k inputs
 var recursiveAsyncConfig = function () {
     let config_msg = 'Select CPU from ';
     //~ There is probably a better way to do this
-    simulator.supported_cpus.forEach(cpu_choice => config_msg += cpu_choice + ' ');
-    
+    simulator.supported_cpus.forEach( cpu_choice => config_msg += cpu_choice + ' ' );
+
     rl.question( config_msg, (text_input) => {
-        if( simulator.supported_cpus.includes( text_input ) ) {
+        if( simulator.supported_cpus.has( text_input ) ) {
             console.log( `CPU selected: ${ text_input }` );
             console.log( 'Simuation Start' )
-            recursiveAsyncReadLine();
+            environment = simulator.factory( text_input ); //~ this creates a global var, right?
+            recursiveAsyncReadLine( environment );
         } else {
             console.log( 'Sorry, input not recognized' );
             recursiveAsyncConfig();
@@ -32,14 +51,38 @@ var recursiveAsyncConfig = function () {
     });
 };
 
-recursiveAsyncConfig();
-
 //~ So how does this shit not break the stack?
 var recursiveAsyncReadLine = function ( environment ) {
     rl.question( '>> ', function ( text_input ) {
-        if( text_input == "exit" || text_input == "quit" )
+        if( exit_commands.has( text_input ) )
             return rl.close();
-        console.log( simulator.parse( text_input ) ); //simulator(text_input);
-        recursiveAsyncReadLine();
+        else {
+            if( recognized_console_commands.has( text_input ) )
+                handle_console_command( environment, text_input );
+            else {
+                let input_opt = environment.parser( text_input ).opt;
+                if( simulator.supported_instructions.has( input_opt ) ){
+                    simulator.execute( environment, text_input );
+                    recursiveAsyncReadLine( environment );
+                }
+                else {
+                    console.log( `Error: instruction '${ text_input }' not recognized.` );
+                }
+            }
+        }
+        
     });
 };
+
+function handle_console_command( environment, text_input ) {
+    console_commands[ text_input ]( environment );
+    recursiveAsyncReadLine( environment );
+}
+
+function show_registers( environment ) {
+    for( const property in environment.registers ) {
+        console.log( `${ property }: ${ environment.registers[ property ] }` );
+    }
+}
+
+recursiveAsyncConfig();
